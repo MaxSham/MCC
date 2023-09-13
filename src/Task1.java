2d13f75f28bb6e47ba7ac21c2855ecad811f9992
MaxSham <cvitimax@gmail.com>
2023-09-13 14:50:38 +0300

    Task1

---
+++ src/Task1.java
@@ -0,0 +1,32 @@
+import Telemetry.Config;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+
+public class Task1 {
+    public static void run(int amountOfLines, int weight){
+        if(weight <= 0 || amountOfLines <= 0) return;
+
+        try(FileInputStream stream = new FileInputStream(Config.KNPFilePath)) {
+            FileWriter fileWriter = new FileWriter("Data/out/Task1.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+
+            byte[] bytes = new byte[weight];
+            int currentLineNum = 0;
+
+            while(stream.read(bytes) != -1 && currentLineNum < amountOfLines){
+                System.out.print(String.format("%04X  ", currentLineNum * weight));
+                for(int i = 0; i < weight; ++i){
+                    System.out.print(String.format("%02X ", bytes[i]));
+                }
+                System.out.print("\n");
+                currentLineNum++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
